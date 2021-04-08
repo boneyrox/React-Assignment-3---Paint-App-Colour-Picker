b19404789e695d72182a6aeeba0d7db6996dd98a
BONEY <boney003.upadhyay@gmail.com>
2021-04-08 18:47:41 +0530

    fr

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,19 @@
 
+import React, { useState } from "react";
+function Selection(props) {
+    const [background, backgroundSet] = useState({ background: "" });
+    const updateSelectionStyle = (background) => {
+        backgroundSet(background);
+    };
+
+    return (
+        <div
+            className="fix-box"
+            style={background}
+            onClick={() => props.applyColor(updateSelectionStyle)}
+        >
+            <h2 className="subheading">Selection</h2>
+        </div>
+    );
+}
+export default Selection;
